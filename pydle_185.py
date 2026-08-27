84c8b722934a83d6093e7557b1df12522737ab33
Prof. Caique <kiqsrb@gmail.com>
2026-08-27 01:10:27 -0300

    Resposta do Desafio 185

---
+++ pydle_185.py
@@ -0,0 +1,9 @@
+l, o, v = ['22', '04', '14'], 'orange', ''
+p, r, n = pydle, range, int
+
+for x in r(5): p(x, 0, v, 'yellow')
+
+for x in r(5): p(x, 3, v, o)
+for i in l: p(n(i[0]), n(i[1]), v, o)
+
+for x in r(2, 4): p(x, 4, v, 'brown')
